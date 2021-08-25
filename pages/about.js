d5fbe8be6b7f6255328fa69b8342b6304d08f32b
Craig Puxty <craig_puxty@talktalk.net>
2021-08-25 20:39:36 +0100

    Global styles

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div className="container">
+      <h2>About page</h2>
+      <div className="content">
+        <button className="btn btn-primary">Primary</button>
+        <button className="btn btn-success">Success</button>
+      </div>
+    </div>
+  );
+};
+
+export default About;
