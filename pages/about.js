ea852d38b98f99d0271bcef23551dbcda9f2025b
Craig Puxty <craig_puxty@talktalk.net>
2021-08-25 21:13:43 +0100

    SASS support

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
-import styles from "../styles/About.module.css";
+import styles from "../styles/About.module.scss";
 
 const About = () => {
   return (
     <div className="container">
-      <h2 className={styles.highlight}>About page</h2>
+      <h2 className={styles.highlightscss}>About page</h2>
       <div className="content">
         <button className="btn btn-primary">Primary</button>
         <button className="btn btn-success">Success</button>
